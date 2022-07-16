20c84e46613f52caddff104570b9a8de00c09ad3
derekhcchou <derekchc53@gmail.com>
2022-07-16 15:06:38 -0700

    add api

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -4,34 +4,33 @@ import { FORM, FORM_TYPE, PrescreenForm, TechScreenForm } from "../types/forms";
 // const endpoint =
 // "https://1syp4w9c5h.execute-api.us-east-1.amazonaws.com/prod/form-events";
 const endpoint = "http://localhost:3000/local/";
-const saveUrl = endpoint + "form-events";
-const getUrl = endpoint + "prescreenRetriever";
+const prescreenUrl = endpoint + "prescreen";
 
 export const getPrescreenData = async (candidateId: string) => {
   let prescreenData;
   try {
     const response: AxiosResponse = await axios.get(
-      `${getUrl}?candidateId=${candidateId}`
+      `${prescreenUrl}?candidateId=${candidateId}`
     );
-    prescreenData = response.data?.body;
+    prescreenData = response.data.body;
   } catch (err) {
     console.error("Error getting prescreen data", err);
   }
   return prescreenData;
 };
 
-export const saveForm = async (
+export const savePrescreenForm = async (
   formType: FORM_TYPE,
   form: PrescreenForm | TechScreenForm
 ) => {
   try {
     const response: AxiosResponse = await axios.post(
-      `${saveUrl}?formType=${FORM[formType].type}`,
+      `${prescreenUrl}?formType=${FORM[formType].type}`,
       form
     );
     return response.data;
   } catch (err) {
-    console.error("Error getting prescreen data", err);
+    console.error("Error saving prescreen data", err);
     throw err;
   }
 };
