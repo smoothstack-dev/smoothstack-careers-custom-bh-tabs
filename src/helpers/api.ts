18cbce2f0235ac5f990e8f9d9b9714109153a030
derekhcchou <derekchc53@gmail.com>
2022-07-17 21:35:51 -0700

    endpoint udpate

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosResponse } from "axios";
 import { FORM, FORM_TYPE, PrescreenForm, TechScreenForm } from "../types/forms";
 
-// const endpoint = "https://1syp4w9c5h.execute-api.us-east-1.amazonaws.com/prod/";
-const endpoint = "http://localhost:3000/local/";
+const endpoint = "https://1syp4w9c5h.execute-api.us-east-1.amazonaws.com/prod/";
+// const endpoint = "http://localhost:3000/local/";
 const prescreenUrl = endpoint + "prescreen";
 
 export const getPrescreenData = async (candidateId: string) => {
